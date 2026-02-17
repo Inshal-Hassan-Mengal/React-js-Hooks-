2143a7674d54fe38617b8e6917db436525e389e5
Inshal Hassan Mengal <inshalh221@gmail.com>
2026-02-17 16:56:08 +0500

    Props completed

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+// import Card from "./components/Card";
+
+
+// function App() {
+//   return (
+//     <div style={{ display: "flex" }}>
+//       <Card
+//         name="inshal"
+//         job="student"
+//         image="https://www.deadgoodbooks.co.uk/wp-content/uploads/2022/02/First-Look-Reacher-1024x682.png"
+//         age="18"
+//         isstudent="yes"
+//       />
+
+//       <Card
+//         name="inshal"
+//         job="student"
+//         image="https://www.deadgoodbooks.co.uk/wp-content/uploads/2022/02/First-Look-Reacher-1024x682.png"
+//         age="19"
+//         isstudent="no"
+//       />
+
+//       <Card
+//         name="inshal"
+//         job="student"
+//         image="https://www.deadgoodbooks.co.uk/wp-content/uploads/2022/02/First-Look-Reacher-1024x682.png"
+//         age="0202020"
+//         isstudent="perhaps"
+//       />
+
+
+
+
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,29 @@
+// function Card(props) {
+//   return (
+//     <div style={props.styles.card}>
+//       <img src={props.image} alt={props.name} style={props.styles.image} />
+//       <h2>{props.name}</h2>
+//       <p>{props.job}</p>
+//       <div>{props.age}</div>
+//       <p>{props.isstudent ? "yes" : "no"}</p>
+//     </div>
+//   );
+// }
+
+
+// const styles = {
+//   card: {
+//     width: "250px",
+//     padding: "15px",
+//     borderRadius: "10px",
+//     boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
+//     textAlign: "center",
+//     margin: "10px",
+//   },
+//   image: {
+//     width: "100%",
+//     borderRadius: "10px",
+//   },
+// };
+
+// export default Card
